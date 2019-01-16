c8b8a62e968f79ed00971a907587f90a9fb337f6
Jacob Smith <jasmith2016@yahoo.com>
2019-01-15 21:56:34 -0800

    Introduce Location Object

---
+++ src/map/Location.java
@@ -0,0 +1,20 @@
+package map;
+
+public class Location {
+	double lat;
+	double lon;
+	
+	public Location(double lat, double lon) {
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	public double getLat() {
+		return lat;
+	}
+	
+	public double getLon() {
+		return lon;
+	}
+
+}
